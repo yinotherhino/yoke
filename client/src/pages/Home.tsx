86c8dafb282cc2df0eac6b1c0ae63c3790812a22
Mona Lisa <muhammedbayero@gmail.com>
2023-02-12 09:56:00 +0100

    responsiveness

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Login from '../components/Login/Login';
 import Signup from '../components/Login/Signup';
 import Navbar from '../components/Navbar/Navbar';
 import { DataContext } from '../context/DataContext';
 import { AllContext } from '../context/types';
+import Dashboard from './Dashboard';
 
 const Home = () => {
-  const {showForm} = useContext(DataContext) as AllContext;
+  const {showForm, isLoggedIn} = useContext(DataContext) as AllContext;
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if (isLoggedIn){
+      navigate("/dashboard")
+    }
+  },[])
   return (<>
     <Navbar />
     {showForm==="login" ? <Login /> : <Signup />}
